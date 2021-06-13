e2c2d80923ab1b78cb6f8b251b50a8d7172d2f14
Camila Queiroz <camimq85@gmail.com>
2021-06-13 18:25:24 -0300

    Adiciona middleware para rota da home_lista de voluntários.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const loginRouter = require('./routes/login');
 const usersRouter = require('./routes/users');
+const homeListaRouter = require('./routes/home_lista');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', loginRouter);
 app.use('/users', usersRouter);
+app.use('/home', homeListaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
