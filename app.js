a4ee6a39148607127e602f978fe954f9189d48b6
Camila Queiroz <camimq85@gmail.com>
2021-06-15 19:16:43 -0300

    Finaliza CSS para página home_lista.

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', loginRouter);
 app.use('/', adminRouter);
-// app.use('/home', homeListaRouter);
 app.use('/lista_vol', homeListaRouter);
 
 // catch 404 and forward to error handler
